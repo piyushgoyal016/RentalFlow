bc55751433497a1b44b888bfb1abd5a37fc8eeac
Manav Panchal <manavpanchal373@gmail.com>
2026-07-18 13:57:54 +0530

    fix: adjust prisma client import path in db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../../generated/prisma/client.js";
+import { PrismaClient } from "../../generated/prisma/client.js";
 import { config } from "./env.js";
 
 const prisma =
